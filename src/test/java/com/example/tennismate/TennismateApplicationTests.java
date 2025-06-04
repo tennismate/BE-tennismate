4e49942cccb3f55323b25eb440b0caf7187f41bf
Reddyong <d0209hong@naver.com>
2025-06-04 22:13:22 +0900

    feat : 프로젝트 생성 commit

---
+++ src/test/java/com/example/tennismate/TennismateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennismate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennismateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
