b44b78667123e075ac508e552fbbdf8a7ccc7d62
Reddyong <d0209hong@naver.com>
2025-06-25 01:31:11 +0900

    #9 feat : 회원가입 성공 테스트 코드 작성
    * 컨트롤러 로직이 정상 호출되고 알맞은 응답이 내려오는지만 판단하는 테스트 코드
    
    * 회원 가입 api 가 동작하게 될 때, 올바른 data 와 message, status 가 내려오는지 판단.

---
+++ src/test/java/com/example/tennismate/member/controller/MemberControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.tennismate.member.controller;
+
+import com.example.tennismate.member.application.service.MemberService;
+import com.example.tennismate.member.dto.request.MemberRegisterRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private MemberService memberService;
+
+    @Test
+    @DisplayName(value = "회원가입 성공")
+    void memberRegisterSuccess() throws Exception {
+        // given
+        MemberRegisterRequest request = MemberRegisterRequest.of(
+                "test@email.com",
+                "testpw1234!!",
+                "testNickname",
+                "010-1111-2222",
+                25
+        );
+
+        // when
+        doNothing().when(memberService).register(request);
+
+        // then
+        mockMvc.perform(post("/api/v1/members/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("요청이 성공했습니다."))
+                .andExpect(jsonPath("$.data").value("회원가입이 완료되었습니다."));
+    }
+}
