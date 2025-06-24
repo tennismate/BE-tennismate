681418e555aa0c9f875686ab82710236e920aa35
Reddyong <d0209hong@naver.com>
2025-06-24 13:36:20 +0900

    #3 feat : 회원가입 기능 구현 완료.
    * 닉네임 중복 검증 과정.
    * 패스워드 암호화 과정.
    * Member 객체 생성 후 저장.

--- src/main/java/com/example/tennismate/global/exception/errorcode/ErrorCode.java
+++ src/main/java/com/example/tennismate/global/exception/errorcode/ErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     // member
-    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.")
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임 입니다.")
     ;
 
     private final HttpStatus status;
