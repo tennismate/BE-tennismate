742e5e9e1456891f2f89a105153a6cd52787db1d
Reddyong <d0209hong@naver.com>
2025-07-03 15:13:24 +0900

    #9 feat : 중복 이메일 테스트 작성
    * api 호출 시, 중복 이메일 관련 에러처리가 올바르게 동작하는지 content type 과 message 를 통해 검증.

--- src/test/java/com/example/tennismate/member/controller/MemberControllerTest.java
+++ src/test/java/com/example/tennismate/member/controller/MemberControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.tennismate.member.controller;
 
+import com.example.tennismate.global.exception.custom.DuplicatedException;
+import com.example.tennismate.global.exception.errorcode.ErrorCode;
 import com.example.tennismate.member.application.service.MemberService;
 import com.example.tennismate.member.dto.request.MemberRegisterRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,7 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -56,7 +60,28 @@ public class MemberControllerTest {
                 .andExpect(jsonPath("$.data").value("회원가입이 완료되었습니다."));
     }
 
-    // TODO : 중복 이메일 예외처리 검증
+    @Test
+    @DisplayName(value = "중복 이메일 테스트")
+    void givenMemberRequest_whenDuplicatedException_thenCheckAPIDataMessage() throws Exception {
+        // given
+        MemberRegisterRequest request = MemberRegisterRequest.of(
+                "duplicated@email.com",
+                "testPw1234!!",
+                "testNickName",
+                "010-1111-2222",
+                25
+        );
+
+        // when
+        doThrow(new DuplicatedException(ErrorCode.DUPLICATED_EMAIL)).when(memberService).register(any(MemberRegisterRequest.class));
+
+        // then
+        mockMvc.perform(post("/api/v1/members/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                ).andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value("이미 존재하는 이메일입니다."));
+    }
 
     // TODO : 중복 닉네임 예외처리 검증
 
