013223df5e58125c92eae1f1026c343f01e58f1d
Reddyong <d0209hong@naver.com>
2025-07-11 16:15:53 +0900

    #9 feat : 닉네임 중복 검증 리포지토리 테스트 작성
    * 중복된 닉네임이 존재하는 경우, 존재하지 않는 경우 각각에 대한 테스트 케이스 작성.

--- src/test/java/com/example/tennismate/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/tennismate/member/repository/MemberRepositoryTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(value = {MemberRepositoryAdapter.class, TestQuerydslConfig.class})
+@DisplayName(value = "회원 리포지토리 테스트")
 public class MemberRepositoryTest {
 
     @Autowired
@@ -81,6 +82,44 @@ public class MemberRepositoryTest {
 
         // then
         assertThat(isExistingEmail).isFalse();
+
+    }
+
+    @Test
+    @DisplayName(value = "중복 닉네임 검증 테스트")
+    void givenMember_whenExistingNickname_thenThrowException() {
+        // given
+        Member member = Member.of(
+                "test@email.com",
+                "testPassword123!",
+                "existNickname",
+                "010-1111-2222",
+                25,
+                null,
+                null,
+                MemberRole.ROLE_USER
+        );
+
+        // when
+        memberRepository.save(member);
+        boolean existNickname = memberRepositoryAdapter.existsByNickname("existNickname");
+
+        // then
+        assertThat(existNickname).isTrue();
+
+    }
+
+    @Test
+    @DisplayName(value = "중복되지 않은 닉네임 검증 테스트")
+    void given_when_then() {
+        // given
+        // 닉네임 정보가 존재하지 않는 상황이기 때문에, Member 객체 저장 x
+
+        // when
+        boolean nonexistNickname = memberRepositoryAdapter.existsByNickname("nonexistNickname");
+
+        // then
+        assertThat(nonexistNickname).isFalse();
 
     }
 
