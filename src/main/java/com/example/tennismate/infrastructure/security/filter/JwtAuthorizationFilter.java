537cda5a560ca2e2a841269c8c7a6e715300f94c
Reddyong <d0209hong@naver.com>
2025-09-17 20:16:46 +0900

    #15 feat : JwtAuthorizationFilter 구현 완료
    * 토큰에서 사용자의 정보 추출.
    * 추출한 정보로 사용자 정보 조회.
    * 인증 객체 생성 후, SecurityContextHolder 에 인증 정보 저장.
    * 인증 처리 실패 시, 401 에러 반환.

--- src/main/java/com/example/tennismate/infrastructure/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/example/tennismate/infrastructure/security/filter/JwtAuthorizationFilter.java
@@ -1,22 +1,32 @@
 package com.example.tennismate.infrastructure.security.filter;
 
+import com.example.tennismate.global.response.ApiResponse;
 import com.example.tennismate.infrastructure.security.jwt.JwtProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
     private final UserDetailsService userDetailsService;
+    private final ObjectMapper objectMapper;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 1. 요청 헤더에서 토큰 추출
@@ -30,19 +40,33 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
             // 4. 인증 처리
             try {
-                // TODO : 4-1. 토큰의 이메일로 DB 에서 사용자 정보 조회
+                // 4-1. 토큰의 이메일로 DB 에서 사용자 정보 조회
+                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
-                // TODO : 4-2. 인증 객체 생성
+                // 4-2. 인증 객체 생성
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-                // TODO : 4-3. SecurityContext 에 인증 정보 저장
-
+                // 4-3. SecurityContext 에 인증 정보 저장
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                context.setAuthentication(authentication);
+                SecurityContextHolder.setContext(context);
 
             } catch (Exception e) {
+                log.error("Cannot set user authentication: {}", e.getMessage());
+                SecurityContextHolder.clearContext();   // 1. 보안 컨텍스트 초기화 - 혹시 모를 비정상적인 인증 정보가 남아있지 않도록, 현재 요청의 보안 정보를 깨끗하게 비운다.
 
+                // 2. HTTP 상태 코드 설정
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+                // 3. 응답 형식 및 내용 설정
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().write(objectMapper.writeValueAsString(
+                        ApiResponse.error(401, "인증 정보가 유효하지 않습니다.")
+                ));
+
+                return;
             }
         }
-
-
 
         // 5. 다음 필터로 요청 전달
         filterChain.doFilter(request, response);
