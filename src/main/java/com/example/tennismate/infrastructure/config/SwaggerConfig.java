a63886f1a0173ae2f75c2826405efb725d88fc01
Reddyong <d0209hong@naver.com>
2025-09-20 14:39:51 +0900

    #16 feat : SecurityRequirement 객체 생성
    * 스웨거의 모든 API 에 인증 정보가 필요하다는 보안 객체 생성.

--- src/main/java/com/example/tennismate/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/example/tennismate/infrastructure/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.tennismate.infrastructure.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +21,8 @@ public class SwaggerConfig {
                 // 1. SecurityScheme 이름 정의
                 String jwtSchemeName = "jwtAuth";
 
-                // TODO : 2. API 요청 헤더에 인증 정보 포함을 위한 SecurityRequirement 객체 생성
+                // 2. API 요청 헤더에 인증 정보 포함을 위한 SecurityRequirement 객체 생성
+                SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
                 // TODO : 3. SecuritySchemes 객체 설정
 
