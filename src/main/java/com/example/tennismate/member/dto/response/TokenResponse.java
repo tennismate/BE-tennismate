96d064f4c97371814561d6c42db4537744b91920
Reddyong <d0209hong@naver.com>
2025-09-20 17:35:37 +0900

    #16 feat : 토큰 재발급 기능 응답에 필요한 record 생성
    * accessToken, refreshToken 을 속성으로 가진다.

--- src/main/java/com/example/tennismate/member/dto/response/TokenResponse.java
+++ src/main/java/com/example/tennismate/member/dto/response/TokenResponse.java
@@ -1,4 +1,10 @@
 package com.example.tennismate.member.dto.response;
 
-public record TokenResponse() {
+public record TokenResponse(
+        String accessToken,
+        String refreshToken
+) {
+    public static TokenResponse of(String accessToken, String refreshToken) {
+        return new TokenResponse(accessToken, refreshToken);
+    }
 }
